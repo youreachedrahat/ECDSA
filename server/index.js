f54b105e68cc12499ac94486e23f9e47eed47cd2
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2023-10-24 17:37:14 +0530

    concurrently

--- server/index.js
+++ server/index.js
@@ -65,8 +65,17 @@ app.post("/send", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  });
+}
+app.listen(port, (Error) => {
+  console.log(`Application listening on PORT ${port}`);
 });
 
 function setInitialBalance(address) {
