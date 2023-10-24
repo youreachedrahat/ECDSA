f0ac5a96309778c64b09cd6e5aab242d628e00d8
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2023-10-24 19:03:34 +0530

    path update from build to dist

--- server/index.js
+++ server/index.js
@@ -74,10 +74,10 @@ app.post("/send", (req, res) => {
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
   });
 }
 app.listen(PORT, (Error) => {
